825daefc94bb35a0b111c0d333368eff41deb143
ctcisar <ctcisar@gmail.com>
2024-10-18 17:17:09 -0700

    more blocking tests

--- src/SoftSeg/test.py
+++ src/SoftSeg/test.py
@@ -9,7 +9,13 @@ def test():
     fig.colorbar(im, ax=ax)
     plt.show(block=False)
 
-    for i in range(10**1000):
+    fig, ax = plt.subplots()
+    im = ax.scatter(range(100, 0, -1), range(100))
+    ax.set_xlabel("ayooo")
+    fig.colorbar(im, ax=ax)
+    plt.show(block=False)
+
+    for i in range(10**10):
         random.random()
         if i % 100 == 0:
             print(i)
